86babf46dfbdc6ca5fa43f7c31940b0c4031a4c1
bps78 <90796928+bps78@users.noreply.github.com>
2023-02-16 11:07:28 -0600

    Initial Commit

---
+++ app/config/colors.js
@@ -0,0 +1,10 @@
+export default{
+    primary: '#87ba52',
+    primaryMid: '#729d44',
+    primaryDark: '#5A7c36',
+    neutral: '#d9d9d9',
+    secondary: '#CC2936',
+    secondaryMid: '#C31a1a',
+    secondaryDark: '#BB0d0d',
+    
+}
