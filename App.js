2caa94c4eb38d53dd0f7c19b6b5405841ecf3f78
bps78 <90796928+bps78@users.noreply.github.com>
2023-02-23 11:02:56 -0600

    API Implementation (DIS & APPL Example)

--- App.js
+++ App.js
@@ -1,4 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
+import { useEffect, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import handler from './app/ApiHandler.js';
 
@@ -6,10 +7,26 @@ import handler from './app/ApiHandler.js';
 
 export default function App() {
   handler()
-  
+  const [aaplPrice, setApplPrice] = useState(0);
+  const [disPrice, setDisPrice] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      let curApplPrice = global.aaplPrice;
+      let curDisPrice = global.disPrice;
+
+       if(curDisPrice != 0) setDisPrice(parseFloat(curDisPrice).toFixed(2)); //Calls these 2 lines every x seconds
+       if(curApplPrice != 0) setApplPrice(parseFloat(curApplPrice).toFixed(2)); 
+
+    }, 1500);
+    return () => clearInterval(interval);
+  }, []);
+    
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Appl: {aaplPrice}</Text>
+      <Text>Dis: {disPrice}</Text>
       <StatusBar style="auto" />
     </View>
   );
--- app/ApiHandler.js
+++ app/ApiHandler.js
@@ -1,23 +1,36 @@
 
-
-
-var aaplPrice = 0;
-
 const handler =  function() {
-  console.log('Handler Started');
 
 
+  var aaplPrice = 0;
+  var disPrice = 0;
+  global.aaplPrice = aaplPrice;
+  global.disPrice = disPrice;
+
+  //console.log('Handler Started');
 
   const socket = new WebSocket('wss://ws.finnhub.io?token=cfnqd0pr01qr96uomd9gcfnqd0pr01qr96uomda0');
   
   socket.addEventListener('open', function (event){
     socket.send(JSON.stringify({'type':'subscribe', 'symbol':'AAPL'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol':'DIS'}))
   });
 
   socket.addEventListener('message', function (event){
     const obj = JSON.parse(event.data);
-    console.log(obj.data[0].s, obj.data[0].p);
-    aaplPrice = obj.data[0].p;
+  
+    try{
+      if(obj.data[0].p != '0' && obj.data[0].s == 'AAPL'){
+        global.aaplPrice = obj.data[0].p;
+      }
+
+      if(obj.data[0].p != '0' && obj.data[0].s == 'DIS'){
+       global.disPrice = obj.data[0].p;
+      }
+    }catch (e){
+      console.log('Handler: No data')
+    }
+    //console.log(obj.data[0].s, global.aaplPrice);
   });
 
   
